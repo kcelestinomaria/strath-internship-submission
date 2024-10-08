d965b304c3510d78ade38f3c94e2cccdd27559b9
unknown <kcelestinomaria127@gmail.com>
2024-10-08 16:31:25 +0300

    Django Data Models created for File & Directory

--- files/models.py
+++ files/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class CurrentDirectory(models.Model):
+class Directory(models.Model):
+    """Django Model to represent a directory in the local filesystem"""
     name = models.CharField(max_length=255)
-    parent = models.ForeignKey('self', null=True, blank=True, related_name=)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='sub-directories', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class File(models.Model):
+    """Django Model to represent a file in the filesystem."""
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='files/')
+    Directory = models.ForeignKey(Directory, related_name='files', on_delete=models.CASCADE)
+    size = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
